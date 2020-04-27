12855ec881a0b20643084f81fc0e9dd9b65ea739
Kent <kfuruie@gmail.com>
2020-04-27 00:23:08 -0400

    working tilt thresholds

--- presenter.py
+++ presenter.py
@@ -1,6 +1,6 @@
-#import tkinter as tk
 import requests
-from presentation_gui import PresentationAPP
+import time
+#from presentation_gui import PresentationAPP
 
 WIDTH = 1000
 HEIGHT = 800
@@ -9,6 +9,7 @@ HEIGHT = 800
 PP_ADDRESS = "http://10.0.0.33:8080"
 PP_CHANNELS = ["accX", "accY", "accZ", "gyrX", "gyrY", "gyrZ"]
 
+#For receiving values from Phyphox
 accX = []
 accY = []
 accZ = []
@@ -17,8 +18,6 @@ gyrX = []
 gyrY = []
 gyrZ = []
 
-
-#window = tk.Tk()
 
 
 def getSensorData():
@@ -48,15 +47,60 @@ def getData():
     return [naccX, naccY, naccZ, ngyrX, ngyrY, ngyrZ]
 
 def main():
+
+    #For initial calibration
+    calibratedX = 0
+    calibratedY = 0
     
-    app = PresentationAPP(WIDTH, HEIGHT)
-    app.show_slides("FORWARD")
-    app.run()
+
+    # GUI Init
+    #app = PresentationAPP(WIDTH, HEIGHT)
+    #app.show_slides("FORWARD")
+    #app.run()
 
     while True:
         [naccX, naccY, naccZ, ngyrX, ngyrY, ngyrZ] = getData()
-        print("Accelerometer: ", naccX, ' ', naccY, ' ', naccZ, ' \n',
-              "Gyroscope: ", ngyrX, ' ', ngyrY, ' ', ngyrZ)
+
+        #Very basic calibration
+        if calibratedX == 0 and calibratedY == 0:
+            for i in range(300):
+                calibratedX += float(naccX)
+                calibratedY += float(naccY)
+                time.sleep(.01)
+
+            calibratedX /= 300
+            calibratedY /= 300
+            print("Calibrated accX: ", calibratedX)
+            print("Calibrated accY: ", calibratedY)
+            print("Calibrated and ready!")
+
+
+        # Tilt Check
+          # If change in accx spikes positive and gyrz spikes negative tilt right
+          # If change in accx spikes negative and gyrz spikes positive tilt left
+              #Backwards needs some work with better identification
+        if (naccX > (calibratedX + 7) and ngyrZ < -6):
+        #if (ngyrZ < -6):
+            print("Current accX: ", naccX)
+            print("Current ngyrZ: ", ngyrZ)
+            print("Going forwards!")
+            #app.show_slides("FORWARD")
+            time.sleep(0.6)
+        elif (naccX < (calibratedX - 7) and ngyrZ > 6 ):
+        #elif (ngyrZ > 6):
+            print("Current accX: ", naccX)
+            print("Current ngyrZ: ", ngyrZ)
+            print("Goimg backwards!")
+            #app.show_slides("BACKWARD")
+            time.sleep(0.6)
+
+        # Squeeze
+            # Take the ngyrX, ngyrY, ngyrZ measurements and smoosh them together into one variable
+            # If this variable reaches a certain threshold, squeeze is detected
+        everyGyr = (ngyrX * ngyrY * ngyrZ)
+
+        print("Master Gyroscope value: ", everyGyr)
+        
         
         # EXAMPLE USE OF PRESENTATION APP
         # ON TILT RIGHT -> app.show_slides("FORWARD")
